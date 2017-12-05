562645c00ea4b3caabf87a66ca05875c57c02adf
Maarten Bobbeldijk <maarten@bobbeldijk.nl>
2017-12-05 07:40:15 +0100

    Added first solution for day 1

---
+++ Day 5/Part 1/Console_Application/CPUJumper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Console_Application
+{
+    public class CPUJumper
+    {
+        private List<int> jumpTable;
+
+        public CPUJumper(List<int> jumpTable)
+        {
+            this.jumpTable = jumpTable;
+        }
+
+        public int ResolveJumpTable()
+        {
+            int currentCPUStep = 0;
+            int jumpTableIndex = 0;
+
+            while (jumpTableIndex < jumpTable.Count)
+            {
+                jumpTableIndex += jumpTable[jumpTableIndex]++;
+
+                currentCPUStep++;
+            }
+            
+            return currentCPUStep;
+        }
+    }
+}
---
+++ Day 5/Part 1/Console_Application/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Console_Application
+{
+    internal class Program
+    {
+        private const string FileLocation = "../../../../input/input.txt";
+        
+        public static void Main(string[] args)
+        {
+            CPUJumper cpuJumper = new CPUJumper(OpenInputFile(FileLocation).Select(int.Parse).ToList());
+            
+            Console.WriteLine(cpuJumper.ResolveJumpTable());
+        }
+
+        private static IEnumerable<string> OpenInputFile(string relativeFileLocation)
+        {
+            String[] seperators = {"\r\n", "\r", "\n"};
+
+            TextReader textReader = File.OpenText(FileLocation);
+            
+            String[] result = File.OpenText($"{Environment.CurrentDirectory}/{relativeFileLocation}")
+                .ReadToEnd()
+                .Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            
+            textReader.Close();
+
+            return result;
+        }
+    }
+}
