7209e0dac9126ba94b73e588f85c725798cf1ad5
Maarten Bobbeldijk <maarten@bobbeldijk.nl>
2017-12-11 19:16:10 +0100

    Added solution for day 11

---
+++ Day 11/Part 1/Console_Application/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Console_Application
+{
+    internal class Program
+    {
+        private const string FileLocation = "../../../../input/input.txt";
+
+        public static void Main(string[] args)
+        {
+            HexMazeSolver hexMazeSolver = new HexMazeSolver();
+
+            OpenInputFile(FileLocation).First().Split(',').ToList().ForEach(direction => hexMazeSolver.AddStep(direction));
+            
+            Console.WriteLine($"Answer: {hexMazeSolver.AmountOfStepsBack()}");
+        }
+
+        private static IEnumerable<string> OpenInputFile(string relativeFileLocation)
+        {
+            String[] seperators = {"\r\n", "\r", "\n"};
+
+            TextReader textReader = File.OpenText(FileLocation);
+
+            String[] result = File.OpenText($"{Environment.CurrentDirectory}/{relativeFileLocation}")
+                .ReadToEnd()
+                .Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+
+            textReader.Close();
+
+            return result;
+        }
+    }
+}
---
+++ Day 11/Part 2/Console_Application/HexMazeSolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_Application
+{
+    public class HexMazeSolver
+    {
+        private readonly Dictionary<string, (int, int)> movementManipulationPerDirection = new Dictionary<string, (int, int)>();
+        
+        private int xPosition;
+        private int yPosition;
+
+        private int furthestAway;
+
+        public HexMazeSolver()
+        {
+            movementManipulationPerDirection.Add("n", (2, 0));
+            movementManipulationPerDirection.Add("ne", (1, 1));
+            movementManipulationPerDirection.Add("se", (-1, 1));
+            movementManipulationPerDirection.Add("s", (-2, 0));
+            movementManipulationPerDirection.Add("sw", (-1, -1));
+            movementManipulationPerDirection.Add("nw", (1, -1));
+        }
+
+        public void AddStep(string step)
+        {
+            if (!movementManipulationPerDirection.ContainsKey(step))
+                throw new Exception($"step '{step}' is not recongized");
+
+            (int, int) positionManipulation = movementManipulationPerDirection[step];
+
+            xPosition += positionManipulation.Item1;
+            yPosition += positionManipulation.Item2;
+
+            int amountOfStepsBack = AmountOfStepsBack();
+            if (amountOfStepsBack > furthestAway)
+                furthestAway = amountOfStepsBack;
+        }
+
+        public int AmountOfStepsBack()
+        {
+            return (Math.Abs(xPosition) + Math.Abs(yPosition)) / 2;
+        }
+
+        public int FurthestAway => furthestAway;
+    }
+}
